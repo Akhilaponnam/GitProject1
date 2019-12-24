65a6e66c2dfa3340aaa9dedbfd5e83483f474d34
Akhilaponnam <akhilaponnam@gmail.com>
2019-12-24 11:44:44 +0530

    Hello

---
+++ sum.java
@@ -0,0 +1,9 @@
+Class A
+{
+	public static void main(String args[])
+	{
+		int a=10,b=20,c=30;
+		c=a+b+c;
+		System.out.println("Sumof three numbers is:+c");
+	}
+}
---
+++ sum2.java
@@ -0,0 +1,12 @@
+
+Class Sum2
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=20;
+		int c=20;
+		c=a+b+c;
+		System.Out.println("Som of three numbers is"+c);
+	}
+}
